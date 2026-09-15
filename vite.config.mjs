d186781d3f27b5b26fa03408325f2be4abe5509c
khaerilaril098 <khaerilaril098@gmail.com>
2026-09-16 01:54:49 +0800

    Delete vite.config.mjs

--- vite.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-    base: '/Belajar_tailwind/',
-    plugins: [
-        tailwindcss()
-    ]
-})
